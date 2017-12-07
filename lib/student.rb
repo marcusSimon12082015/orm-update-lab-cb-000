d9d0698257b51a2fb7830f12ff1d0cf00e93c031
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-07 20:50:54 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -50,4 +50,9 @@ class Student
     obj.grade = row[2]
     obj
   end
+  def find_by_name(name)
+    sql = <<-SQL 
+      SELECT * FROM students WHERE name = ?
+    SQL
+    DB[:conn].execute(sql,name) 
 end
