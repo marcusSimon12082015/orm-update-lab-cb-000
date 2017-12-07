ca3329a3856e5734b70b8283569115c193961bc6
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-07 20:32:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -6,7 +6,7 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def initialize(name,grade,id=nil)
-    @name = name 
+    @name = name
     @grade = grade
   end
 end
