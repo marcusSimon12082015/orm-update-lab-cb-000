e56dee89d3b24b8c9568f49ff026a7e8aa9ba34d
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2017-12-07 20:43:24 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,9 +33,9 @@ class Student
     end
   end
   def update
-    sql = <<-SQL 
+    sql = <<-SQL
       UPDATE students SET name = ?,grade = ? WHERE id = ?
     SQL
-    DB[:conn].execute(sql,self.name,self.grade,self.id) 
-  end 
+    DB[:conn].execute(sql,self.name,self.grade,self.id)
+  end
 end
